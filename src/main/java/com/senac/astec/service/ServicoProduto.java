e6a5b47c5ceb41097c63483d53d217b7bb35343f
Ana Carolina <Ana Carolina@192.168.100.5>
2018-04-29 23:15:24 -0300

    Alteração da classe ServicoProduto

--- src/main/java/com/senac/astec/service/ServicoProduto.java
+++ src/main/java/com/senac/astec/service/ServicoProduto.java
@@ -10,15 +10,15 @@ import java.io.IOException;
 import java.util.List;
 //Classe de Servico de Produto
 public class ServicoProduto {
-    ImovelDAO produtoDAO = new ImovelDAO();
+    ImovelDAO imovelDAO = new ImovelDAO();
     
-    public void cadastrarProduto(Imovel produto) throws IOException {
+    public void cadastrarProduto(Imovel imovel) throws IOException {
         
         
 
         try {
             //Realiza a chamada de inserção na fonte de dados
-            produtoDAO.inserirProduto(produto);
+            imovelDAO.inserirImovel(imovel);
         } catch (Exception e) {
             //Imprime qualquer erro técnico no console e devolve
             //uma exceção e uma mensagem amigável a camada de visão
@@ -26,12 +26,12 @@ public class ServicoProduto {
         }
     }
 
-    //Atualiza um produto na fonte de dados
-    public void atualizarProduto(Imovel produto) throws IOException {
+    //Atualiza um imovel na fonte de dados
+    public void atualizarProduto(Imovel imovel) throws IOException {
         
 
         try {
-            produtoDAO.updateProduto(produto);
+            imovelDAO.updateImovel(imovel);
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -40,7 +40,7 @@ public class ServicoProduto {
 
     public  List<Imovel> procurarProduto(String nome, int codigoempresa) throws IOException {
         try {
-        return produtoDAO.listarProduto(nome, codigoempresa);
+        return imovelDAO.listarImoveis(codigoempresa);
           
         } catch (Exception e) {
             //Imprime qualquer erro técnico no console e devolve
@@ -52,7 +52,7 @@ public class ServicoProduto {
 
     public Imovel encontrarProdutoPorNome(String nome, int codigoempresa) throws IOException {
         try {
-            return produtoDAO.encontrarProduto(nome, codigoempresa);
+            return imovelDAO.encontrarImovel(nome, codigoempresa);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -61,7 +61,7 @@ public class ServicoProduto {
     
     public Imovel encontrarProdutoPorCodigo(int codigo, int codigoempresa) throws IOException {
         try {
-            return produtoDAO.encontrarProdutoCodigo(codigo, codigoempresa);
+            return imovelDAO.encontrarImovelCodigo(codigo, codigoempresa);
         } catch (Exception e) {
             e.printStackTrace();
             return null;
@@ -69,7 +69,7 @@ public class ServicoProduto {
     }
     public boolean encontrarProdutoCadastro(String nome, int codigoempresa) throws IOException {
         try {
-            return produtoDAO.encontrarProdutoCadastro(nome, codigoempresa);
+            return imovelDAO.encontrarImovelCadastro(nome, codigoempresa);
         } catch (Exception e) {
             e.printStackTrace();
             return false;
@@ -78,24 +78,24 @@ public class ServicoProduto {
 
     public void excluirProduto(Integer codigo, int codigoempresa) throws IOException {
         try {
-            produtoDAO.deletarProduto(codigo, codigoempresa);
+            imovelDAO.deletarImovel(codigo, codigoempresa);
         } catch (Exception e) {
             System.out.println("Erro");
         }
     }
     
-    public void atualizaEstoque(int codigo, int estoque) throws IOException {
+    public void atualizaEstoque(Imovel imovel) throws IOException {
         try {
-            produtoDAO.atualizarEstoque(codigo, estoque);
+            imovelDAO.updateImovel(imovel);
         } catch (Exception e) {
             System.out.println("Erro");
         }
     }
     
-    //Lista produtos de determinada empresa
+    //Lista imovels de determinada empresa
     public List<Imovel> listarProdutos(int codigoempresa) throws IOException, Exception {
         try {
-            return produtoDAO.listarProdutos(codigoempresa);
+            return imovelDAO.listarImoveis(codigoempresa);
             
         } catch (Exception e) {
             e.printStackTrace();
@@ -103,10 +103,10 @@ public class ServicoProduto {
         }
     }
     
-    //Lista produtos Totais
+    //Lista imovels Totais
     public List<Imovel> listarProdutostotais() throws IOException, Exception {
         try {
-            return produtoDAO.listarProdutostotais();
+            return imovelDAO.listarImoveistotais();
             
         } catch (Exception e) {
             e.printStackTrace();
